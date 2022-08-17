a4f3b09b118e0429f48f3c4c6514aa20f6db0fc9
MooonDrop <nowyouseeme406@gmail.com>
2022-08-17 18:50:22 +0200

    added configuration file for fast deploy

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined("VG_ACCESS") || die("Access denied"); 
+
+const SITE_URL = 'http://Market_PHP';
+const PATH = "/";
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'Market';
