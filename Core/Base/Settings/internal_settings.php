7f3db2031c410349128343858a2b99ae360c3be7
MooonDrop <nowyouseeme406@gmail.com>
2022-08-17 18:59:29 +0200

    added configuration file with internal setting constants

---
+++ Core/Base/Settings/internal_settings.php
@@ -0,0 +1,19 @@
+<?php 
+
+defined("VG_ACCESS") || die("Access denied");
+
+const TEMPLATE = 'Templates/default/';
+const ADMIN_TEMPLATES = 'Core/Admin/Views/';
+
+const COOKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const ADMIN_COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
