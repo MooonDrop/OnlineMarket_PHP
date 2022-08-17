2f97afe5bc7614b7f42aa16f13e85105a84cd758
MooonDrop <nowyouseeme406@gmail.com>
2022-08-17 18:46:37 +0200

    added access key, header with content type and started session

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define("VG_ACCESS", true);
+
+header('Content-type:text/html;charset=utf-8'); 
+session_start();
+
+require_once "config.php";
+require_once "Core/Base/Settings/internal_settings.php";
